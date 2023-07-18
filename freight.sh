b8575ef07ec178a104956b123ca548080576907b
Jordan Ocokoljic <jordan.ocokoljic@chilled.vc>
2023-07-18 17:16:14 +1000

    Add `sweep` command

--- freight.sh
+++ freight.sh
@@ -5,11 +5,19 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+case "$1" in
+    'sweep')
+        git branch | grep 'freight-cargo/' | xargs git branch -d
+        exit 0
+    ;;
+esac
+
 if [ -z "$2" ]; then
     echo 'usage: freight <command> <tag>'
     exit 1
 fi
 
+c="$(git branch --show-current)"
 b="freight-cargo/$2"
 
 case "$1" in
@@ -23,7 +31,8 @@ case "$1" in
         git add .
         git commit -m '[freight wip]'
         git push origin "$b"
-        git checkout master
+        git checkout "$c"
+        exit 0
     ;;
     'accept')
         if ! git ls-remote --exit-code --head origin "$b" &> /dev/null; then
@@ -35,8 +44,9 @@ case "$1" in
         git checkout "$b"
         git merge origin/"$b"
         git reset --soft HEAD~1
-        git checkout master
+        git checkout "$c"
         git push -d origin "$b"
         git branch -d "$b"
+        exit 0
     ;;
 esac
