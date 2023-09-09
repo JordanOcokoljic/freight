9e3a812264ed35a9582f0fe307c01ed43f352751
Jordan Ocokoljic <jordan.ocokoljic@chilled.vc>
2023-09-09 17:40:37 +1000

    Update freight script to have help menu
    Also removed redundant section of the readme

--- freight.sh
+++ freight.sh
@@ -1,52 +1,140 @@
 #!/usr/bin/env sh
 
-if [ -z "$1" ]; then
-    echo 'usage: freight <command> <tag>'
-    exit 1
-fi
+freight_help()
+{
+    case "$1" in
+        'send')
+            cat << END
+usage: freight send <tag>
 
-case "$1" in
-    'sweep')
-        git branch | grep 'freight-cargo/' | xargs git branch -d
-        exit 0
-    ;;
-esac
+The send command will create a new branch with a name following the
+template 'freight-cargo/<tag>', freight will then add all changes in
+the repository and commit them with the message '[freight wip]',
+the branch will then be pushed to origin.
+END
+        ;;
+        'accept')
+            cat << END
+usage: freight accept <tag>
 
-if [ -z "$2" ]; then
-    echo 'usage: freight <command> <tag>'
-    exit 1
-fi
+The accept command will attempt to pull a branch from origin with a
+name following the template 'freight-cargo/<tag>', freight will then
+record the current branch before checking out the pulled branch,
+resetting the '[freight wip]' commit, and going back to the current
+branch - bringing along the sent changes. Once this is finished,
+freight will delete the branch from origin.
+END
+        ;;
+        'sweep')
+            cat << END
+usage: freight sweep
 
-c="$(git branch --show-current)"
-b="freight-cargo/$2"
+The sweep command will clean up all branches that start with the prefix
+'freight-cargo/' to reduce the number of excess branches around. Note
+that if a branch has been pulled with 'freight accept <tag>' then there
+is a the possiblity that the only copy of that branch as it stands is
+on the device, so exercise caution when sweeping branches.
+END
+        ;;
+        *)
+        cat << END
+usage: freight <command> [<args>]
 
-case "$1" in
-    'send')
-        if git ls-remote --exit-code --heads origin "$b" &> /dev/null; then
-            echo 'tag already exists in origin'
+There are four commands that freight accepts:
+   help     Shows this menu.
+   send     Pushes all current changes to the remote on the given tag.
+   accept   Pulls all pushed changes from the remote on the given tag.
+   sweep    Delete any local branches left over from sends.
+
+Any text value can be used as a tag - but short and memorable values
+are suggested.
+
+See 'freight help <command>' to read about the specifics of how the
+commands work and what side effects, if any, they have.
+END
+        ;;
+    esac
+}
+
+freight_send()
+{
+    if [ -z "$1" ]; then
+        echo 'freight: no tag provided'
+        exit 1
+    fi
+
+    current="$(git branch --show-current)"
+    branch="freight-cargo/$1"
+
+    if git ls-remote --exit-code --heads origin "$branch" &> /dev/null; then
+        echo 'freight: tag already exists in origin'
+        exit 1
+    fi
+
+    git checkout -b "$branch"
+    git add -A
+    git commit -m '[freight wip]'
+    git push origin "$branch"
+    git checkout "$current"
+}
+
+freight_accept()
+{
+    if [ -z "$1" ]; then
+        echo 'freight: no tag provided'
+        exit 1
+    fi
+
+    current="$(git branch --show-current)"
+    branch="freight-cargo/$1"
+
+    if ! git ls-remote --exit-code --head origin "$branch" &> /dev/null; then
+        echo 'freight: tag does not exist in origin'
+        exit 1
+    fi
+
+    git fetch origin "$branch"
+    git checkout "$branch"
+    git merge origin/"$branch"
+    git reset --soft HEAD~1
+    git checkout "$current"
+    git push -d origin "$branch"
+    git branch -D "$branch"
+}
+
+freight_sweep()
+{
+    git branch | grep 'freight-cargo/' | xargs git branch -D
+}
+
+freight_noncommand()
+{
+    echo "freight: '$1' is not a freight command. See 'freight help'."
+}
+
+freight_main()
+{
+    case "$1" in
+        'help')
+            freight_help "$2"
+        ;;
+        'send')
+            freight_send "$2"
+        ;;
+        'accept')
+            freight_accept "$2"
+        ;;
+        'sweep')
+            freight_sweep
+        ;;
+        '')
+            freight_help
             exit 1
-        fi
+        ;;
+        *)
+            freight_noncommand "$1"
+        ;;
+    esac
+}
 
-        git checkout -b "$b"
-        git add .
-        git commit -m '[freight wip]'
-        git push origin "$b"
-        git checkout "$c"
-        exit 0
-    ;;
-    'accept')
-        if ! git ls-remote --exit-code --head origin "$b" &> /dev/null; then
-            echo 'tag does not exist in origin'
-            exit 1
-        fi
-
-        git fetch origin "$b"
-        git checkout "$b"
-        git merge origin/"$b"
-        git reset --soft HEAD~1
-        git checkout "$c"
-        git push -d origin "$b"
-        git branch -d "$b"
-        exit 0
-    ;;
-esac
+freight_main "$@"
--- readme.md
+++ readme.md
@@ -4,19 +4,3 @@ Freight is a tool for quickly sharing work in progress code within `git`
 repositories between people or devices. It does so by leveraging the existing
 features within `git` ensuring compatability with both remote hosts and local
 clients.
-
-
-## Usage
-
-`freight <command> [tag]`
-
-Freight supports three commands:
-- `send` will push any changes to the remote, associating them with the
-  provided _freight tag_.
-- `accept` will pull any changes from the remote associated with the provided
-  _freight tag_.
-- `sweep` will delete any local branches that were created as a side effect of
-  using `send`.
-
-Any text value can be used as a _freight tag_ - just ensure that is memorable,
-and short enough to be easily typed.
